27ba98bd644c331c35d9d7dbb41ff0a12b42f45f
Karol Rechman <Karol.Rechman@adesso.de>
2023-11-24 10:13:23 +0100

    delete "1" files

--- Catalog/Catalog.Application/Handlers/UpdateProductHandler.cs
+++ Catalog/Catalog.Application/Handlers/UpdateProductHandler.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Catalog.Application.Commands;
+using Catalog.Core.Entities;
+using Catalog.Core.Repositories;
+using MediatR;
 
-namespace Catalog.Application.Handlers
+namespace Catalog.Application.Handlers;
+
+public class UpdateProductHandler : IRequestHandler<UpdateProductCommand, bool>
 {
-	internal class UpdateProductHandler
+	private readonly IProductRepository _productRepository;
+
+	public UpdateProductHandler(IProductRepository productRepository)
 	{
+		_productRepository = productRepository;
+	}
+	public async Task<bool> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+	{
+		bool productEntity = await _productRepository.UpdateProduct(new Product
+		{
+			Id = request.Id,
+			Description = request.Description,
+			ImageFile = request.ImageFile,
+			Name = request.Name,
+			Price = request.Price,
+			Summary = request.Summary,
+			Brands = request.Brands,
+			Types = request.Types
+		});
+
+		return productEntity;
 	}
 }
